53083e733bef0a812917e403348ee0602689526c
ineedoritos <cgon991@gmail.com>
2023-03-04 14:21:11 -0600

    no se probanding
    asdfsadf

--- secciones/citas/index.php
+++ secciones/citas/index.php
@@ -1,37 +1,5 @@
-<?php include("../../conexionBd.php");?>
-<?php include("../../templates/header.php");?>
+<?php include("../../templates/header.php"); ?>
 
-<div class="container d-flex justify-content-center">
-    <h1>Citas médicas</h1>
-</div>
-
-<div class="card m-auto">
-    <div class="card-header d-flex justify-content-center">
-        <a name="" id="" class="btn btn-primary" href="crearVet.php" role="button">agendar Cita</a>
-    </div>
-    <div class="card-body">
-
-    <div class="table-responsive-sm">
-    <table class="table table text-center">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">motivo</th>               
-                <th scope="col">fecha</th>
-                <th scope="col">hora</th>
-                <th scope="col">mascota</th>
-                <th scope="col">veterinario</th>
-                <th scope="col">acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            
-            
-        </tbody>
-    </table>
-</div>
-    </div>
-
-</div>
+<h2>citas</h2>
 
 <?php include("../../templates/footer.php"); ?>
---
+++ testing/login.php
@@ -0,0 +1,43 @@
+<?php
+include("../templates/header.php")
+?>
+
+
+<body class="justify-content-center">
+    <div class=" justify-content-xl-center rounded ">
+        <div class="card text-center">
+            <div class="card-header ">
+                Login
+            </div>
+
+            <div class="card-body">
+                <form action="" method="post">
+                    
+                <div class=" d-flex flex-column text-center p-2">
+                    <label for="" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+
+                </div>
+
+                    <div class=" d-flex flex-column text-center p-2">
+                        <label for="" class="form-label">Contraseña </label>
+                        <input type="password" class="form-control" name="" id="" placeholder="">
+                    </div>
+
+                </form>
+                
+            </div>
+        </div>
+
+    </div>
+
+
+    </div>
+
+
+
+
+    </div>
+</body>
+
+</html>
